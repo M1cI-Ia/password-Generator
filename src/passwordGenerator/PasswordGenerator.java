d1c0b7c04efa159e9b9f5e6cee31898fefde2960
micha <michael-ee@albus.me>
2021-12-03 23:42:45 +0100

    1st steps

--- src/passwordGenerator/PasswordGenerator.java
+++ src/passwordGenerator/PasswordGenerator.java
@@ -11,25 +11,35 @@ public class PasswordGenerator {
 
         //checking options: passwordGenerator 4 f
         int numberOfPasswordsWanted = 1;
+        int passLength = 16;
         boolean wantsFile = false;
 
-        if (args.length == 2) {
+
+        if (args.length == 3) {
             try {
                 numberOfPasswordsWanted = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
                 System.out.println("Number of passwords not recognized.");
-                e.printStackTrace();
+                //e.printStackTrace();
+            }
+            try {
+                passLength = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e){
+                System.out.println("Desired password length not recognized.");
+                //e.printStackTrace();
             }
 
-            if (args[1].equals("f")) {
+            if (args[2].equals("f")) {
                 wantsFile = true;
             }
         }
 
         System.out.println("Generating " + numberOfPasswordsWanted + " password(s)");
         System.out.println("saving them to a file:" + wantsFile);
-
+        System.out.println("Length " + passLength);
+        //warn at to short pws
         //generationg PWs
+        //show extimation of no. of bits
         //output
 
 
