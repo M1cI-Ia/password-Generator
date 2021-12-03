b813d4ee1fb400a72eb0520cece34f6c91edbe66
micha <michael-ee@albus.me>
2021-12-03 23:32:29 +0100

    1st steps

--- src/passwordGenerator/PasswordGenerator.java
+++ src/passwordGenerator/PasswordGenerator.java
@@ -1,7 +1,37 @@
 package passwordGenerator;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class PasswordGenerator {
+
+
     public static void main(String[] args) {
+
+
+        //checking options: passwordGenerator 4 f
+        int numberOfPasswordsWanted = 1;
+        boolean wantsFile = false;
+
+        if (args.length == 2) {
+            try {
+                numberOfPasswordsWanted = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Number of passwords not recognized.");
+                e.printStackTrace();
+            }
+
+            if (args[1].equals("f")) {
+                wantsFile = true;
+            }
+        }
+
+        System.out.println("Generating " + numberOfPasswordsWanted + " password(s)");
+        System.out.println("saving them to a file:" + wantsFile);
+
+        //generationg PWs
+        //output
+
 
     }
 }
