8bea0b236019056d3e98baffea704e3454a62614
micha <michael-ee@albus.me>
2021-12-04 16:54:08 +0100

    reworked options handling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # password-Generator
+
 This is a simple password generator that I’m doing for fun and learning.
+
+Usage: passwordGenerator [NumberOfPasswords LengthOfPasswords [f]]
+f...print PWs to file
--- src/passwordGenerator/PasswordGenerator.java
+++ src/passwordGenerator/PasswordGenerator.java
@@ -1,47 +1,61 @@
 package passwordGenerator;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class PasswordGenerator {
-
+    private static int numberOfPasswordsWanted = 1;
+    private static int passLength = 15;
+    private static boolean wantsFile = false;
 
     public static void main(String[] args) {
 
 
-        //checking options: passwordGenerator 4 f
-        int numberOfPasswordsWanted = 1;
-        int passLength = 15;
-        boolean wantsFile = false;
+        //checking options: passwordGenerator <numberOfPasswords> <desiredLength> <f=print them to a file>
 
+        if (args.length == 0) {
+            //TODO: continue with default settings
+            System.out.println("Using default settings");
+        } else if (args.length == 2) {
+            getDesiredSettings(args);
 
-        if (args.length == 3) {
-            try {
-                numberOfPasswordsWanted = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.out.println("Number of passwords not recognized.");
-                //e.printStackTrace();
-            }
-            try {
-                passLength = Integer.parseInt(args[1]);
-            } catch (NumberFormatException e){
-                System.out.println("Desired password length not recognized.");
-                //e.printStackTrace();
-            }
+        } else if (args.length == 3) {
+            getDesiredSettings(args);
 
             if (args[2].equals("f")) {
                 wantsFile = true;
+            } else {
+                printUsageInfo();
+                System.exit(0);
             }
+        } else {
+            printUsageInfo();
+            System.exit(0);
         }
 
         System.out.println("Generating " + numberOfPasswordsWanted + " password(s)");
         System.out.println("saving them to a file:" + wantsFile);
         System.out.println("Length " + passLength);
-        //warn at to short pws
+        //TODO:warn at to short pws
+
         //generationg PWs
+
+
         //show extimation of no. of bits
         //output
 
 
+    }
+
+    protected static void getDesiredSettings(String[] arr) {
+
+        try {
+            numberOfPasswordsWanted = Integer.parseInt(arr[0]);
+            passLength = Integer.parseInt(arr[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("number of passwords or desired length not recognized");
+        }
+    }
+
+    protected static void printUsageInfo() {
+        //TODO
+        System.out.println("usage");
     }
 }
